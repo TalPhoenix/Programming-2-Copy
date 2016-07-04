2f1f029b13a5c79e3752ccf320da0382cd1987da
TalPhoenix <v-lentz@onu.edu>
2016-07-04 00:30:43 -0400

    update

--- src/Assignments/Assignment_10_InheritanceUI.java
+++ src/Assignments/Assignment_10_InheritanceUI.java
@@ -5,8 +5,11 @@
  */
 package Assignments;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.Date;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -31,19 +34,30 @@ public class Assignment_10_InheritanceUI extends JFrame{
     private static String currentTime(String zone){
         return Instant.now().atZone(ZoneId.of( zone )).toString();   
     }
+
+    private static int getHours(String zone ){
+        return Instant.now().atZone(ZoneId.of( zone )).getHour();
+    }
+
+    private static int getMinutes(String zone ){
+        return Instant.now().atZone(ZoneId.of( zone )).getMinute();
+    }
     
+    private static String getTime(String zone ){
+        return getHours(zone) + ":" + getMinutes(zone);
+    }
+
     private static void showClockUI(){
      JFrame myFrame = new JFrame("Test GUI");
         myFrame.setVisible(true);
         myFrame.setBounds(300, 200, 1400, 900);
-               
-        JLabel myText = new JLabel("Current EST time is: " + currentTime("-4"));
+        JLabel myText = new JLabel("Current EST time is: " + getTime("-4"));
         myText.setBounds(300, 500, 500, 200);
-        JLabel nyTime = new JLabel("Current New York time is: " + currentTime("America/New_York"));
+        JLabel nyTime = new JLabel("Current New York time is: " + getTime("America/New_York"));
         nyTime.setBounds(300, 400, 500, 200);
-        JLabel mcTime = new JLabel("Current Mexic City time is: " + currentTime("America/Mexico_City"));
+        JLabel mcTime = new JLabel("Current Mexic City time is: " + getTime("America/Mexico_City"));
         mcTime.setBounds(300, 300, 500, 200);
-        JLabel tTime = new JLabel("Current Tokyo time is: " + currentTime("Asia/Tokyo"));
+        JLabel tTime = new JLabel("Current Tokyo time is: " + getTime("Asia/Tokyo"));
         tTime.setBounds(300, 200, 500, 200);
         JLabel blank = new JLabel("");
         blank.setBounds(300, 100, 500, 200);
